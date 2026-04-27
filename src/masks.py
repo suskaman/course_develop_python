60069d84d39510e53cc6bbeb4d5c4217054f53f8
dushecjka <suskaman@mail.ru>
2026-04-27 22:36:03 +0200

    formatted modules and fix a little problems

--- src/masks.py
+++ src/masks.py
@@ -1,24 +1,18 @@
-from typing import Union
 import logging
+from typing import Union
 
 # create logger
 mask_logger = logging.getLogger("app.masks")
-file_handler = logging.FileHandler('../logs/masks.log', mode='w')
-console_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(console_formatter)
+file_handler = logging.FileHandler("../course_develop_python/logs/masks.log", mode="w", encoding='utf-8')
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
 mask_logger.addHandler(file_handler)
 mask_logger.setLevel(logging.DEBUG)
 
-# logging.basicConfig(level=logging.DEBUG,
-#                     format='%(asctime)s - %(filename)s - %(levelname)s - %(message)s',
-#                     filename='../logs/masks.log',
-#                     filemode='w')
-#
-# mask_logger = logging.getLogger("app.masks")
 
-def get_mask_card_number(card_number: Union[int, str]) -> str | None:
+def get_mask_card_number(card_number: Union[int, str]) -> str:
     """changing card number with mask"""
-    mask_logger.info(f"start to create a mask for card number")
+    mask_logger.info("start to create a mask for card number")
 
     mask_str = "XXXX XX** **** XXXX"
     mask_list = list(mask_str)
@@ -35,23 +29,22 @@ def get_mask_card_number(card_number: Union[int, str]) -> str | None:
                 counter += 1
             elif mask_list[i] == "*":
                 counter += 1
-
         mask_card_number = "".join(mask_list)
 
-        mask_logger.info(f"a mask for card number is created")
+        mask_logger.info("a mask for card number is created")
         return mask_card_number
 
     except IndexError as ex:
         mask_logger.error(f"error happened: {ex}", exc_info=True)
+        return ""
 
     finally:
-        mask_logger.info(f"end getting mask card number")
-
+        mask_logger.info("end getting mask card number")
 
 
 def get_mask_account(account_number: Union[int, str]) -> str:
     """changing account number with mask"""
-    mask_logger.info(f"start to create a mask for account number ")
+    mask_logger.info("start to create a mask for account number ")
 
     mask_str = "**XXXX"
     mask_list = list(mask_str)
@@ -72,11 +65,15 @@ def get_mask_account(account_number: Union[int, str]) -> str:
 
         mask_account_number = "".join(mask_list)
 
-        mask_logger.info(f"a mask for account number is created")
+        mask_logger.info("a mask for account number is created")
         return mask_account_number
 
     except IndexError as ex:
         mask_logger.error(f"error happened: {ex}", exc_info=True)
+        return ""
 
     finally:
-        mask_logger.info(f"end getting mask account number")
+        mask_logger.info("end getting mask account number")
+
+if __name__ == "__main__":
+    get_mask_card_number(1234123412341234)
--- src/utils.py
+++ src/utils.py
@@ -2,27 +2,27 @@ import json
 import logging
 
 # create logger
-util_logger = logging.getLogger("app.utilss")
-file_handler = logging.FileHandler('../logs/utils.log', mode='w')
-console_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(console_formatter)
+util_logger = logging.getLogger("app.utils")
+file_handler = logging.FileHandler("../course_develop_python/logs/utils.log", mode="w", encoding='utf-8')
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
 util_logger.addHandler(file_handler)
 util_logger.setLevel(logging.DEBUG)
 
 
 def get_transactions(path_to_json: str) -> list:
     """return a list of all transactions"""
-    util_logger.info('start getting transactions')
+    util_logger.info("start getting transactions")
     try:
         with open(path_to_json, "r", encoding="utf-8") as f:
-            util_logger.info('opened json file')
+            util_logger.info("opened json file")
 
             data = json.load(f)
 
             if not isinstance(data, list):
                 raise ValueError("json file must contain a list of transactions")
 
-            util_logger.info('transactions are got')
+            util_logger.info("transactions are got")
             return data
 
     except FileNotFoundError:
@@ -33,14 +33,9 @@ def get_transactions(path_to_json: str) -> list:
         util_logger.error("invalid json file", exc_info=True)
         return [{}]
 
-    except ValueError :
-        util_logger.error(f"json file must contain a list of transactions", exc_info=True)
+    except ValueError:
+        util_logger.error("json file must contain a list of transactions", exc_info=True)
         return [{}]
 
     finally:
-        util_logger.info('end getting transactions')
-
-
-
-if __name__ == "__main__":
-    print(get_transactions("../data/operations.json"))
+        util_logger.info("end getting transactions")
