89d68adcff247f14a9f93a70f414eb67c8d9df7b
dushecjka <suskaman@mail.ru>
2026-04-30 22:14:29 +0200

    add tests for data_loading.py

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,13 +1,18 @@
-# from unittest.mock import patch
-# from src import data_loader
-# import pandas as pd
-#
-#
-# def test_get_data_from_csv():
-#     with patch('src.data_loader.pd.read_csv') as mock_read_csv:
-#         mock_read_csv.return_value = pd.DataFrame({'one': [1, 2], 'two': [4, 5]})
-#         assert data_loader.get_data_from_excel('test.csv') == [{'one': 1, 'two': 4}, {'one': 2, 'two': 5}]
-#         mock_read_csv.assert_called_once_with('test.csv')
+from unittest.mock import patch
+from src import data_loader
+import pandas as pd
 
 
-# def test_get_data_from_excel():
+def test_get_data_from_csv():
+    with patch('src.data_loader.pd.read_csv') as mock_read_csv:
+        mock_read_csv.return_value = pd.DataFrame({'one': [1, 2], 'two': [4, 5]})
+        assert data_loader.get_data_from_csv('test.csv') == [{'one': 1, 'two': 4}, {'one': 2, 'two': 5}]
+        mock_read_csv.assert_called_once_with('test.csv')
+
+
+def test_get_data_from_excel():
+    with patch('src.data_loader.pd.read_excel') as mock_read_excel:
+        mock_read_excel.return_value = pd.DataFrame({'one': [1, 2], 'two': [4, 5]})
+        assert data_loader.get_data_from_excel('test.xlsx') == [{'one': 1, 'two': 4}, {'one': 2, 'two': 5}]
+        mock_read_excel.assert_called_once_with('test.xlsx')
+        
